c0ddd6401c87db8d223db7794ceb57628252b188
abravo <abravo@acid.cl>
2023-12-30 01:06:00 -0300

    add token to spotify

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
+    production: true,
     url: 'https://api.spotify.com',
     urlToken: 'https://token-spotify.onrender.com/generate-token/a9e99ff29b284aeebef44659cad36214/917c3be6949a433fb6885721ccea3205'
 };
